f795d94ee9402826955bf404e68f7690603e56b3
naubergois <naubergois@gmail.com>
2014-09-10 01:10:49 -0300

    Nova versão da execucao do JMeter

--- src/main/java/br/nauber/jmeter/scriptReader/RunningJMeter.java
+++ src/main/java/br/nauber/jmeter/scriptReader/RunningJMeter.java
@@ -1,11 +1,14 @@
 package br.nauber.jmeter.scriptReader;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.util.Properties;
 
+import org.apache.jmeter.config.Arguments;
 import org.apache.jmeter.engine.StandardJMeterEngine;
+import org.apache.jmeter.reporters.ResultCollector;
+import org.apache.jmeter.reporters.Summariser;
 import org.apache.jmeter.save.SaveService;
+import org.apache.jmeter.testelement.TestPlan;
 import org.apache.jmeter.util.JMeterUtils;
 import org.apache.jorphan.collections.HashTree;
 
@@ -34,7 +37,31 @@ public class RunningJMeter {
 
 		System.out.print(testPlanTree);
 		// Run JMeter Test
+		Properties properties=new Properties();
+		properties.setProperty("ip", "www.vbulletin.com");
+		jmeter.setProperties(properties);
+		
+		
+		Summariser summer = null;
+		String summariserName = JMeterUtils.getPropDefault("summariser.name", "summary");//$NON-NLS-1$
+		if (summariserName.length() > 0) {
+		    summer = new Summariser(summariserName);
+		}
+
+		String logFile = "/Users/naubergois/Downloads/apache-jmeter-2.11/bin/filelognew.jtl";
+		ResultCollector logger = new ResultCollector(summer);
+		logger.setFilename(logFile);
+		testPlanTree.add(testPlanTree.getArray()[0], logger);
+		
+		TestPlan plan= (TestPlan) testPlanTree.getArray()[0];
+		Arguments args1=new Arguments();
+		args1.addArgument("ip", "www.vbulletin.com");
+		//System.out.print("ip "+plan.setUserDefinedVariables(vars));
+		//plan.setProperty("ip", "www.vbulletin.com");
+		plan.setUserDefinedVariables(args1);
+		testPlanTree.getArray()[0]=plan;
 		jmeter.configure(testPlanTree);
+		
 		jmeter.run();
 	}
 
---
+++ src/main/java/br/nauber/screen/HelloWorld.java
@@ -0,0 +1,33 @@
+package br.nauber.screen;
+
+import java.awt.Button;
+import java.awt.event.ActionEvent;
+import java.beans.EventHandler;
+ 
+public class HelloWorld extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+ 
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+
+ Scene scene = new Scene(root, 300, 250);
+
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+ public static void main(String[] args) {
+        launch(args);
+    }
+}
