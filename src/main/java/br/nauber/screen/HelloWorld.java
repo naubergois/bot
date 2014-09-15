c4b79a8c727da7db9d36a10e25d5d928fa9a61d6
naubergois <naubergois@gmail.com>
2014-09-15 01:08:25 -0300

    FX screen first version

--- src/main/java/br/nauber/screen/HelloWorld.java
+++ src/main/java/br/nauber/screen/HelloWorld.java
@@ -1,10 +1,16 @@
 package br.nauber.screen;
 
-import java.awt.Button;
-import java.awt.event.ActionEvent;
-import java.beans.EventHandler;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
  
-public class HelloWorld extends Application {
+public class HelloWorld extends Application{
     
     @Override
     public void start(Stage primaryStage) {
